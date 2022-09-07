5dc206ff97bbbddbb86334c87fc0437a3801d83e
nmcassa <nmcassa804@outlook.com>
2022-09-07 18:24:25 -0400

    Initial commit

---
+++ 2021get.py
@@ -0,0 +1,85 @@
+import re
+import requests
+import json
+from bs4 import BeautifulSoup
+from json import JSONEncoder
+
+class Box:
+	def __init__(self, gameUrl: str) -> None:
+		self.url = gameUrl
+		self.build()
+
+	def build(self) -> None:
+		page = get_parsed_page("https://www.baseball-reference.com/" + self.url)
+		header = page.find("h1").text
+		header = header.split(" at ")
+		header[1] = header[1].split(" Box")
+
+		score = page.findAll("div", {"class": "score"})
+
+		self.home = header[1][0]
+		self.away = header[0]
+
+		self.homeScore = score[1].text
+		self.awayScore = score[0].text
+
+		homeRecord = score[1].parent.next_sibling.text
+		awayRecord = score[0].parent.next_sibling.text
+
+		if self.homeScore > self.awayScore:
+			homeRecord = homeRecord.split('-')
+			homeRecord[0] = int(homeRecord[0])
+			homeRecord[0] -= 1
+			awayRecord = awayRecord.split('-')
+			awayRecord[1] = int(awayRecord[1])
+			awayRecord[1] -= 1
+		elif self.homeScore < self.awayScore:
+			homeRecord = homeRecord.split('-')
+			homeRecord[1] = int(homeRecord[1])
+			homeRecord[1] -= 1
+			awayRecord = awayRecord.split('-')
+			awayRecord[0] = int(awayRecord[0])
+			awayRecord[0] -= 1
+
+		self.homeRecord = str(homeRecord[0]) + "-" + str(homeRecord[1])
+		self.awayRecord = str(awayRecord[0]) + "-" + str(awayRecord[1])
+
+	def jsonify(self) -> str:
+		return json.dumps(self, indent=4,cls=Encoder)
+
+def get_parsed_page(url: str) -> None:
+	headers = {
+		"referer": "https://baseball-reference.com",
+		"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+	}
+
+	return BeautifulSoup(requests.get(url, headers=headers).text, "lxml")
+
+def get_url_list() -> list:
+	page = get_parsed_page("https://www.baseball-reference.com/leagues/majors/2019-schedule.shtml")
+
+	data = page.find("div", {'id': "all_9125626744"})
+	data = data.findAll("em")
+
+	games = []
+
+	for item in data:
+		games.append(item.find("a")['href'])
+
+	return games
+
+class Encoder(JSONEncoder):
+	def default(self, o):
+		return o.__dict__
+
+if __name__ == "__main__":
+	urls = get_url_list()
+
+	f = open("2019.txt", "a")
+
+	for url in urls:
+		bs = Box(url)
+
+		f.write("%s(%s)@%s(%s)^%s#%s\n" % (bs.home, bs.homeRecord, bs.away, bs.awayRecord, bs.homeScore, bs.awayScore))
+
+	f.close()
---
+++ README.md
@@ -0,0 +1,2 @@
+# mlb-game-data
+ A storage for a scraper that can write txt files containing season results
